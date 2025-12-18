24e79510cbeea9dcce5c41754a7636a20d1a6935
izzyfisher <isabel.christine.fisher@gmail.com>
2025-12-18 01:22:49 -0500

    fixes for account information access

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250929_111647 from './20250929_111647'
+import * as migration_20251218_032616 from './20251218_032616'
 
 export const migrations = [
   {
     up: migration_20250929_111647.up,
     down: migration_20250929_111647.down,
     name: '20250929_111647',
+  },
+  {
+    up: migration_20251218_032616.up,
+    down: migration_20251218_032616.down,
+    name: '20251218_032616',
   },
 ]
